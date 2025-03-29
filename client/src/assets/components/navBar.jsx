144482f3532180a6f971e2b5d83bf392a181e1ad
Cody Kha <cody.kha0@gmail.com>
2025-03-29 02:25:02 -0700

    Fixed animations for Navbar elements

--- client/src/assets/components/navBar.jsx
+++ client/src/assets/components/navBar.jsx
@@ -4,22 +4,27 @@ import "./navBar.css";
 
 function NavBar() {
   const handleClick = (event) => {
+    event.preventDefault();
     const icon = event.currentTarget;
+    const anchor = icon.closest("a");
     icon.classList.add("shrink")
     setTimeout(() => {
       icon.classList.remove("shrink");
+      if (anchor && anchor.href) {
+        window.open(anchor.href, anchor.target || "_self");
+      }
     }, 250);
-  };
+  }
   return (
     <>
       <nav className="navbar">
-        <a href="#" className="name">codykha</a>
+        <a href="#" className="name" onClick={handleClick}>codykha</a>
         <ul>
           <li>
             <a href="https://www.linkedin.com/in/cody-kha" target="_blank"><i className="fa-brands fa-linkedin icon1" onClick={handleClick}></i></a>
           </li>
           <li>
-            <a href="https://github.com/codykhadev" target="_blank"><i className="fa-brands fa-square-github icon2"></i></a>
+            <a href="https://github.com/codykhadev" target="_blank"><i className="fa-brands fa-square-github icon2" onClick={handleClick}></i></a>
           </li>
           <li>
             <a href="#"><i className="fa-solid fa-envelope icon3" onClick={handleClick}></i></a>
